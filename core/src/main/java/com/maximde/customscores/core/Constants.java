5a12106b8a8034093e1e913f02a8684d3159a3a5
Maxim <maximdeclient@gmail.com>
2024-04-09 18:46:40 +0200

    Reduced final jar size

--- core/src/main/java/com/maximde/customscores/core/Constants.java
+++ core/src/main/java/com/maximde/customscores/core/Constants.java
@@ -4,6 +4,6 @@ import net.kyori.adventure.text.Component;
 
 public class Constants {
     public static final Component PREFIX = Message.get("<gradient:#11b2c3:#ff6565>[CustomScores] ");
-    public static final String VERSION = "1.2.0";
+    public static final String VERSION = "1.2.1";
     public static final String GLOBAL_COMMAND_NAME = "customscores";
 }
